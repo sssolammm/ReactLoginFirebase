f9eea1ee2c8b08ce374734cade36f330b45f15b4
David Benito <sssolammm@hotmail.com>
2019-02-14 00:31:15 +0100

    try fix

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+// import React from 'react';
+// import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import TabNavigator from './TabNavigator';
+// import { Store } from './Store/Store';
+// import { Provider } from 'react-redux';
+
+
+// <Provider store={Store}>
+//     <TabNavigator></TabNavigator>
+// </Provider>
+
+// export default createAppContainer(TabNavigator);
+
 import React from 'react';
+import { View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import TabNavigator from './TabNavigator';
 import { Store } from './Store/Store';
 import { Provider } from 'react-redux';
 
+const Nav = createAppContainer(TabNavigator);
 
-<Provider store={Store}>
-    <TabNavigator></TabNavigator>
-</Provider>
+export default class App extends React.Component {
 
-export default createAppContainer(TabNavigator);
+  render() {
+    return (
+        <Provider store={Store}>
+        <View style={{flex:1, backgroundColor:'white'}}>
+            <Nav />
+        </View>
+        </Provider>
+    );
+  }
+}
